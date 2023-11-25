e096abe91965027d2c99ec7cb4f47d9d08c681b4
jzywiecki <jakubzywiecki1@gmail.com>
2023-11-25 15:08:08 +0100

    feat: added database

---
+++ server/src/main/java/com/developerex/server/GreetingsController.java
@@ -0,0 +1,14 @@
+package com.developerex.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+        @GetMapping("/greet")
+        public String greet() {
+            return "Hello, World";
+        }
+}
