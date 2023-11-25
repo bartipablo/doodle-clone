98d0ea50510542371f29dab58474f61768038b08
bartipablo <bartek.pawlowski00@gmail.com>
2023-11-25 17:40:32 +0100

    feat: added Room model

--- server/src/main/java/com/developerex/server/room/Room.java
+++ server/src/main/java/com/developerex/server/room/Room.java
@@ -1,4 +1,38 @@
 package com.developerex.server.room;
 
+import com.developerex.server.term.Term;
+import com.developerex.server.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
 public class Room {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private String id;
+
+        private String title;
+
+        private String description;
+
+        private LocalDateTime deadline;
+
+        @ManyToOne
+        private User owner;
+
+        @OneToMany
+        private List<Term> terms;
+
+        @ManyToMany
+        private List<User> participants;
+
 }
