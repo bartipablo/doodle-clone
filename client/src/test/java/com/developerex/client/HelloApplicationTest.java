038b90f0470b473b0dfb534206857f1961478d4c
jzywiecki <jakubzywiecki1@gmail.com>
2023-11-26 14:55:17 +0100

    feat: added tests to client

---
+++ client/src/test/java/com/developerex/client/HelloApplicationTest.java
@@ -0,0 +1,12 @@
+package com.developerex.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloApplicationTest {
+
+    @Test
+    void start() {
+    }
+}
