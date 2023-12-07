9a6527a855ea0e2c3ac822f0bacfe441b18d6810
bartipablo <bartek.pawlowski00@gmail.com>
2023-12-07 22:40:53 +0100

    feat: added TermMapperTest

--- server/src/test/java/com/developerex/server/term/TermMapperTest.java
+++ server/src/test/java/com/developerex/server/term/TermMapperTest.java
@@ -1,7 +1,12 @@
 package com.developerex.server.term;
 
+import com.developerex.server.term.dto.TermDto;
+import com.developerex.server.term.mapper.TermMapper;
+import com.developerex.server.term.model.Term;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,9 +14,57 @@ class TermMapperTest {
 
     @Test
     void mapToDto() {
+        LocalDateTime specificDateTime = LocalDateTime.of(2023, 12, 7, 15, 30);
+
+        //given
+        var entity = Term.builder()
+                .id(1L)
+                .startDateTime(specificDateTime)
+                .duration(10)
+                .build();
+
+        var expectedDto = TermDto.builder()
+                .id(1L)
+                .startDateTime(specificDateTime)
+                .duration(10)
+                .build();
+
+        //when
+        var actualDto = TermMapper.mapToDto(entity);
+
+        //then
+        assertAll(
+                () -> assertEquals(expectedDto.id(), actualDto.id()),
+                () -> assertEquals(expectedDto.startDateTime(), actualDto.startDateTime()),
+                () -> assertEquals(expectedDto.duration(), actualDto.duration())
+        );
     }
 
     @Test
     void mapToEntity() {
+        LocalDateTime specificDateTime = LocalDateTime.of(2023, 12, 7, 15, 30);
+
+        //given
+        var dto = TermDto.builder()
+                .id(1L)
+                .startDateTime(specificDateTime)
+                .duration(10)
+                .build();
+
+        var expectedEntity = Term.builder()
+                .id(1L)
+                .startDateTime(specificDateTime)
+                .duration(10)
+                .build();
+
+        //when
+        var actualEntity = TermMapper.mapToEntity(dto);
+
+        //then
+        assertAll(
+                () -> assertEquals(expectedEntity.getId(), actualEntity.getId()),
+                () -> assertEquals(expectedEntity.getStartDateTime(), actualEntity.getStartDateTime()),
+                () -> assertEquals(expectedEntity.getDuration(), actualEntity.getDuration())
+        );
     }
 }
